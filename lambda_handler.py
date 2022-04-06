513c985e05dadc68a214867b9b0292cdc7613890
Scale-Up SaaS <100524415+saasscaleup@users.noreply.github.com>
2022-04-06 19:00:24 +0300

    Create lambda_handler.py

---
+++ lambda_handler.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3.9
+import urllib3
+import json
+
+http = urllib3.PoolManager()
+
+def lambda_handler(event, context):
+    
+    url = "<slack-webhook-url>"
+    
+    msg = {
+        "channel": "<slack-channel>",
+        "username": "AWS-LAMBDA",
+        "text": event['message']
+    }
+    
+    encoded_msg = json.dumps(msg).encode('utf-8')
+    
+    response = http.request('POST',url, body=encoded_msg)
+    
+    print({
+        "message": event['message'], 
+        "status_code": response.status, 
+        "response": response.data
+    })
+    
+    # return {
+    #     "statusCode": response.status,
+    #     "headers": {
+    #         "Content-Type": "application/json"
+    #     },
+    #     "body": json.dumps({
+    #         "message": event['message'], 
+    #         #"response": response.data
+    #     })
+    # }
